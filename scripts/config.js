1cc3dec73f51a0e1ba21c1263f9e359d009e736b
Siphamandla Mncube <88335645+Siphamandla01@users.noreply.github.com>
2023-10-20 11:45:01 +0200

    using js to config edit, back and confirm button

--- scripts/config.js
+++ scripts/config.js
@@ -5,4 +5,22 @@ function openPlayerConfig() {
 function closePlayerConfig() {
 	playerConfigOverlayElement.style.display = "none";
 	backdropElement.style.display = "none";
+
+	formElement.firstElementChild.classList.remove("error");
+	errorsOutputElement.textContent = "";
+}
+
+function savePlayerConfig(event) {
+	event.preventDefault();
+	const formData = new FormData(event.target);
+	const enteredPlayername = formData.get("username").trim();
+
+	if (!enteredPlayername) {
+		// same as if (enteredPlayername === " ") but written in shorter way
+		playerNameInput = document.getElementById("enter-player-name");
+		playerNameInput.classList.add("error");
+		//event.target.firstElementChid.classList.add("error");
+		errorsOutputElement.textContent = "Please enter a valid name!";
+		return; //this makes sure no other code is returned if this part of code is run.
+	}
 }
