4e4588b54aecdeb6d37da7c70e59ee835f6994d1
Siphamandla Mncube <88335645+Siphamandla01@users.noreply.github.com>
2023-10-20 12:31:46 +0200

    configuring players input

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,6 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+	const selectedPlayerId = +event.target.dataset.playerid; // '+' converts string to an int
+	editedPlayer = selectedPlayerId;
 	playerConfigOverlayElement.style.display = "block";
 	backdropElement.style.display = "block";
 }
@@ -8,6 +10,7 @@ function closePlayerConfig() {
 
 	formElement.firstElementChild.classList.remove("error");
 	errorsOutputElement.textContent = "";
+	formElement.firstChild.lastChild.value = "";
 }
 
 function savePlayerConfig(event) {
@@ -23,4 +26,18 @@ function savePlayerConfig(event) {
 		errorsOutputElement.textContent = "Please enter a valid name!";
 		return; //this makes sure no other code is returned if this part of code is run.
 	}
+
+	const updatePlayerDataElement = document.getElementById(
+		"player-" + editedPlayer + "-data"
+	);
+	updatePlayerDataElement.children[1].textContent = enteredPlayername;
+
+	// if (editedPlayer === 1){
+	//     players[0].name = enteredPlayername
+	// }else{
+	//     players[1].name = enteredPlayername
+	// }
+	players[editedPlayer - 1].name = enteredPlayername;
+
+	closePlayerConfig();
 }
