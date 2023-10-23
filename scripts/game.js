ac2f3fca1cd9b893617f5bb1e04cbd29389cdc84
Siphamandla Mncube <88335645+Siphamandla01@users.noreply.github.com>
2023-10-23 15:06:39 +0200

    tracking selected fields on the game board

--- scripts/game.js
+++ scripts/game.js
@@ -17,7 +17,18 @@ function switchPlayer() {
 }
 
 function selectGameField(event) {
+	const selectedColumn = event.target.dataset.col - 1;
+	const selectedRow = event.target.dataset.row - 1;
+
+	if (gameData[selectedRow][selectedColumn] > 0) {
+		alert("Please select an empty field!");
+		return;
+	}
+
 	event.target.textContent = players[activePlayer].symbol;
 	event.target.classList.add("disabled");
+
+	gameData[selectedRow][selectedColumn] = activePlayer + 1;
+	console.log(gameData);
 	switchPlayer();
 }
